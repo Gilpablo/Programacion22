7d6415e99a294b0fc8b7b39b15075962f2f0f883
gilpablo <gilpablo2000.gpbp@gmail.com>
2022-04-29 12:44:21 +0200

    +

--- EjerciciosProgramacionGraficaArchivo/src/ejercicios/ejercicio8.java
+++ EjerciciosProgramacionGraficaArchivo/src/ejercicios/ejercicio8.java
@@ -58,12 +58,11 @@ public class ejercicio8 extends JFrame {
 	    int columna = 0;
 	    int cont2 =0;
 	    
-	    table.setValueAt(numbers[0], 1, 1);
 	    
-//	    for (int i = 0; i < 9; i++) {
-//	    	table.setValueAt(numbers[i], fila, columna+cont2);
-//	    	cont2 =+ 1;
-//		}
+	    for (int i = 0; i < 10; i++) {
+	    	table.setValueAt(numbers[i], fila, columna+cont2);
+	    	cont2 = cont2 +  1;
+		}
 	    
 	
 	}
@@ -94,19 +93,10 @@ public class ejercicio8 extends JFrame {
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-//				{numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6], numbers[7], numbers[8] ,numbers[9]},
-//				{numbers[10], numbers[11], numbers[12], numbers[13], numbers[14], numbers[15], numbers[16], numbers[17], numbers[18] ,numbers[19]},
-//				{numbers[20], numbers[21], numbers[22], numbers[23], numbers[24], numbers[25], numbers[26], numbers[27], numbers[28] ,numbers[29]},
-//				{numbers[30], numbers[31], numbers[32], numbers[33], numbers[34], numbers[35], numbers[36], numbers[37], numbers[38] ,numbers[39]},
-//				{numbers[40], numbers[41], numbers[42], numbers[43], numbers[44], numbers[45], numbers[46], numbers[47], numbers[48] ,numbers[49]},
-//				{numbers[50], numbers[51], numbers[52], numbers[53], numbers[54], numbers[55], numbers[56], numbers[57], numbers[58] ,numbers[59]},
-//				{numbers[60], numbers[61], numbers[62], numbers[63], numbers[64], numbers[65], numbers[66], numbers[67], numbers[68] ,numbers[69]},
-//				{numbers[70], numbers[71], numbers[72], numbers[73], numbers[74], numbers[75], numbers[76], numbers[77], numbers[78] ,numbers[79]},
-//				{numbers[80], numbers[81], numbers[82], numbers[83], numbers[84], numbers[85], numbers[86], numbers[87], numbers[88] ,numbers[89]},
-//				{numbers[90], numbers[91], numbers[92], numbers[93], numbers[94], numbers[95], numbers[96], numbers[97], numbers[98] ,numbers[99]},
+				{null, null, null, null, null, null, null, null, null, null},
 			},
 			new String[] {
-				"Tabla 1", "Tabla 2 ", "Tabla 3", "Tabla 4", "Tabla 5", "Tabla 6", "Tabla 7", "Tabla 8", "Tabla 9", "Tabla 10"
+				"Tabla 1", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
 			}
 		));
 		table.setBounds(61, 79, 375, 160);
